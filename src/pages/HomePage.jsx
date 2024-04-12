140d0de50b0593bdf2b60bbfc5b39ffecc3fe747
djpb <djpbadenhorst@gmail.com>
2024-04-12 23:23:23 +0200

    Added check for correct url

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,26 @@
-import { Container } from 'd4design';
+import { useState, useEffect } from 'react';
 import { Header } from 'd4design';
+import { Container } from 'd4design';
 import { HiOutlineKey } from "react-icons/hi";
 
-export const HomePage = ({ }) => {  
+
+
+export const HomePage = ({ }) => {
+  const [url, setUrl] = useState(null);
+  useEffect(() => {
+    async function getUrl() {      
+      const url = await fetch('http://desktop.local:80', {mode: 'no-cors'})
+            .then(response => 'http://desktop.local:80').catch(err=>{
+              return fetch('http://192.168.68.122:80', {mode: 'no-cors'})
+                .then(response => 'http://192.168.68.122:80').catch(err=>{
+                  return 'http://100.109.163.106:80';
+                });
+            });
+      setUrl(url);
+    }
+    getUrl();
+  }, []);
+  
   return (
     <div className="h-screen flex justify-center content-center">
       <Container className="m-auto w-1/2">
@@ -16,7 +34,7 @@ export const HomePage = ({ }) => {
         </div>
       </Container>
       <Container className="fixed top-2 right-2 hover:bg-grayscale-50 transform active:scale-90 transition-transform">
-        <a href="http://100.109.163.106">
+        <a href={url}>
           <HiOutlineKey className="p-1 stroke-primary-50 text-3xl"/>
         </a>
       </Container>
